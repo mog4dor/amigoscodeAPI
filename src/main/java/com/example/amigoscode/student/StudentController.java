3ce8a5821ae618fb36329fad2de47e7e48ff3f22
mog4dor <adamkar94@gmail.com>
2023-01-14 22:09:55 +0100

    initialized the local psql databse

--- src/main/java/com/example/amigoscode/student/StudentController.java
+++ src/main/java/com/example/amigoscode/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.amigoscode.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +12,15 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/list")
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Myriame",
-                        "myriame@gmail.com",
-                        LocalDate.of(2002,01,7),
-                        21
-                )
-        );
+        return this.studentService.getStudents();
     }
 }
--- src/main/java/com/example/amigoscode/student/StudentService.java
+++ src/main/java/com/example/amigoscode/student/StudentService.java
@@ -1,9 +1,26 @@
 package com.example.amigoscode.student;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class StudentService {
 
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Myriame",
+                        "myriame@gmail.com",
+                        LocalDate.of(2002,01,7),
+                        21
+                )
+        );
+    }
 
 }
